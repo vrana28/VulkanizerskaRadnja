213e2c3cc01c2c3bd49e8a0363aa8a4d795a6648
vrana28 <vranjesdjordje@gmail.com>
2021-03-21 23:21:27 +0100

    Dodata VulkanizersakaRadnjaTest koj nije skroz zavrsena

---
+++ VulkanizerskaRadnja/src/auto_radnja/gume/VulkanizerskaRadnjaTest.java
@@ -0,0 +1,26 @@
+package auto_radnja.gume;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import auto_radnja.RadnjaTest;
+
+class VulkanizerskaRadnjaTest extends RadnjaTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- VulkanizerskaRadnja/test/auto_radnja/RadnjaTest.java
+++ VulkanizerskaRadnja/test/auto_radnja/RadnjaTest.java
@@ -6,24 +6,62 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import auto_radnja.gume.AutoGuma;
+
 public abstract class RadnjaTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+	protected Radnja radnja;
 
 	@Test
 	void testDodajGumu() {
-		fail("Not yet implemented");
+		AutoGuma a =new AutoGuma();
+		a.setMarkaModela("Guma 1");
+		a.setPrecnik(15);
+		a.setSirina(155);
+		a.setVisina(55);
+		
+		radnja.dodajGumu(a);
+		
+
+		
 	}
 
 	@Test
-	void testPronadjiGumu() {
-		fail("Not yet implemented");
+	void testDodajGumuNull() {
+		assertThrows(java.lang.NullPointerException.class,
+				()-> radnja.dodajGumu(null));
+		
+	}
+	
+	@Test
+	void testDodajGumuDuplikat() {
+		AutoGuma a =new AutoGuma();
+		a.setMarkaModela("Guma 1");
+		a.setPrecnik(15);
+		a.setSirina(155);
+		a.setVisina(55);
+		
+		radnja.dodajGumu(a);
+		
+		AutoGuma b =new AutoGuma();
+		b.setMarkaModela("Guma 1");
+		b.setPrecnik(15);
+		b.setSirina(155);
+		b.setVisina(55);
+		
+		RuntimeException e = assertThrows(java.lang.RuntimeException.class,
+				()-> radnja.dodajGumu(b));
+		assertEquals("Ovakva  guma vec postoji", e.getMessage());
+		
+
+		
+	}
+	
+	@Test
+	void testPronadjiGumuNull() {
+		assertThrows(java.lang.NullPointerException.class,
+				()-> radnja.pronadjiGumu(null));
+		
 	}
 
 }
