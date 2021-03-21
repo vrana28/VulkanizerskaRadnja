b5ad4d117aacf02fee7a2ce358e388245f2c2059
vrana28 <vranjesdjordje@gmail.com>
2021-03-21 23:04:14 +0100

    Ispravljena greska

--- VulkanizerskaRadnja/src/auto_radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/auto_radnja/VulkanizerskaRadnja.java
@@ -8,7 +8,7 @@ import auto_radnja.gume.AutoGuma;
  * Klasa koja implementira interfejs Radnja i ima atribut gume
  * 
  * @author DzoleDzavol
- * @version
+ * @version 1.0
  */
 
 public class VulkanizerskaRadnja implements Radnja {
@@ -22,13 +22,16 @@ public class VulkanizerskaRadnja implements Radnja {
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)throw new NullPointerException("Guma ne sme biti null");
 		if (gume.contains(a))
-			throw new RuntimeException("Guma vec postoji");gume.addFirst(a);
+			throw new RuntimeException("Guma vec postoji");
+		gume.addFirst(a);
 		
 	}
 
 	@Override
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
-		if (markaModel == null)return null;LinkedList<AutoGuma> novaLista = new LinkedList<AutoGuma>();
+		if (markaModel == null)
+			return null;
+		LinkedList<AutoGuma> novaLista = new LinkedList<AutoGuma>();
 		for(int i=0;i<gume.size();i++) {
 			if (gume.get(i).equals(markaModel))
 				novaLista.add(gume.get(i));
---
+++ VulkanizerskaRadnja/test/auto_radnja/RadnjaTest.java
@@ -0,0 +1,29 @@
+package auto_radnja;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public abstract class RadnjaTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testDodajGumu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPronadjiGumu() {
+		fail("Not yet implemented");
+	}
+
+}
